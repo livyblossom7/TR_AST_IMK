3e47a9c91fa6998b92f1db8b83e9220d87c67371
livyblossom7 <disacodi2674@gmail.com>
2026-07-07 06:37:59 +0700

    Perbaikan hubungan antar page

--- js/logout-modal.js
+++ js/logout-modal.js
@@ -5,7 +5,7 @@
     if (!logoutLinks.length) return;
 
     var isInHtmlFolder = window.location.pathname.indexOf('/html/') !== -1;
-    var homeHref = isInHtmlFolder ? '../index.html' : 'index.html';
+    var homeHref = isInHtmlFolder ? 'startedpage.html' : 'html/startedpage.html';
 
     var overlay = document.createElement('div');
     overlay.className = 'logout-modal-overlay';
